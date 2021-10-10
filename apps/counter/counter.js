9450e661de7ca1eff80b739488bd91a1fabc1959
abood871 <92264284+abood871@users.noreply.github.com>
2021-10-10 22:14:08 +0300

    Update counter.js

--- apps/counter/counter.js
+++ apps/counter/counter.js
@@ -1,49 +1,51 @@
+Bangle.setLCDPower(1);
+Bangle.setLCDTimeout(0);
 var counter = 0;
 
 function updateScreen() {
-  g.clearRect(0, 50, 250, 150);
+  g.clearRect(0, 250, 250, 150);
   g.setColor(0xFFFF);
   g.setFont("Vector",40).setFontAlign(0,0);
-  g.drawString(Math.floor(counter), g.getWidth()/2, 100);
-  g.drawString('-', 45, 100);
-  g.drawString('+', 185, 100);
+  //g.drawString(Math.floor(counter), g.getWidth()/2, 100);
+  g.drawString(Math.floor(counter), g.getWidth()/2, 200);
+  g.drawString('-', 45, 200);
+  g.drawString('-', 45, 200);
+  //g.drawString('-', 45, 100);
+  g.drawString('+', 190, 200);
+ // g.drawString('+', 185, 100);
 }
 
 
-// add a count by using BTN1 or BTN5
+
 setWatch(() => {
-  counter += 1;
+  if(counter!=7){
+  counter += 1;}
+  if(counter==7){
+    g.drawImage(require("Storage").read("am.img"),56,27);
+    counter=7;
+  }
   updateScreen();
 }, BTN1, {repeat:true});
 
-setWatch(() => {
-  counter += 1;
-  updateScreen();
-}, BTN5, {repeat:true});
-
-// subtract a count by using BTN3 or BTN4
-setWatch(() => {
-  counter -= 1;
-  updateScreen();
-}, BTN4, {repeat:true});
 
 setWatch(() => {
   counter -= 1;
+  g.clear();
+  if(counter==-1){
+    counter=0;}
   updateScreen();
 }, BTN3, {repeat:true});
 
-// reset by using BTN2
+
 setWatch(() => {
   counter = 0;
+  g.clear();
   updateScreen();
 }, BTN2, {repeat:true});
 
 g.clear(1).setFont("6x8");
-g.drawString('Tap right or BTN1 to increase\nTap left or BTN3 to decrease\nPress BTN2 to reset.', 25, 200);
+//g.drawString(' counter for twaf', 25, 200);
 
 Bangle.loadWidgets();
 Bangle.drawWidgets();
 updateScreen();
-
-// TODO: Enable saving counts to file
-// Add small watch
