a4c6958e9546d95f7726f6178fef7bf7cbb5d06a
abood871 <92264284+abood871@users.noreply.github.com>
2022-05-14 19:58:26 +0300

    Update app.js

--- apps/timer/app.js
+++ apps/timer/app.js
@@ -6,41 +6,42 @@ function updateScreen() {
   g.clearRect(0, 250, 250, 150);
   g.setColor(0xFFFF);
   g.setFont("Vector",40).setFontAlign(0,0);
-  //g.drawString(Math.floor(counter), g.getWidth()/2, 100);
+
   g.drawString(Math.floor(counter), g.getWidth()/2, 200);
   g.drawString('-', 45, 200);
   g.drawString('-', 45, 200);
-  //g.drawString('-', 45, 100);
+
   g.drawString('+', 190, 200);
- // g.drawString('+', 185, 100);
+
 }
 
 
 
 setWatch(() => {
   g.clear();
-  // if(counter!=7){
+
   counter += 1;
   if(counter==33){
-
- g.drawString('Taqabal', 125, 50);
+Bangle.buzz(200, 1);
+ g.drawString('Astaghfir', 125, 50);
  g.drawString('Allah', 125, 100);
 
   }
   if(counter==66){
-
- g.drawString('Taqabal', 125, 50);
+Bangle.buzz(200, 1);
+ g.drawString('Subhan', 125, 50);
  g.drawString('Allah', 125, 100);
 
   }
   if(counter==99){
-
- g.drawString('Taqabal', 125, 50);
+Bangle.buzz(200, 1);
  g.drawString('Allah', 125, 100);
+ g.drawString('Akbar', 125, 50);
+
 
   }
 
-//}
+
   if(counter==101){
 
     counter=0;
@@ -65,13 +66,8 @@ setWatch(() => {
 }, BTN2, {repeat:true});
 
 g.clear(1).setFont("6x8");
-//g.drawString(' Taqabal Allah', 25, 100);
+
 
 Bangle.loadWidgets();
 Bangle.drawWidgets();
 updateScreen();
-
-//g.drawImage(require("Storage").read("am.img"),66,27);
-    //g.clear(1).setFont("6x8");
-   //g.drawString('Taqabal', 125, 50);
-    //g.drawString('Allah', 125, 100);
